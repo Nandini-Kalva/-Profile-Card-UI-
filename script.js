ae290624e5811e5bf5b1db3df3e6c2e8a25e408d
Nandini.Kalva <knandini778070@gmail.com>
2025-09-04 11:46:26 +0530

    Add files via upload
     Features  
    - Simple and responsive design  
    - Profile image, name, role, and short description  
    - Smooth hover transitions using CSS  
    - Interactive action buttons: ❤ Favorite, ✉ Message, 👥 Follow  
    
    
      Tech Stack  
    - HTML5 – Semantic structure  
    - CSS3 – Flexbox, gradients, transitions, shadows  
    - JavaScript(optional) – Button interactivity

---
+++ script.js
@@ -0,0 +1,5 @@
+
+  document.getElementById("favBtn").onclick = () => alert("You liked this profile ❤️");
+  document.getElementById("msgBtn").onclick = () => alert("Message sent 📩");
+  document.getElementById("followBtn").onclick = () => alert("Now following 👥");
+
